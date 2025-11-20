cdb6b5aa212d87f367c5319c2e127285910c9416
Jacob Barazoto <26826868+jacobbarazoto@users.noreply.github.com>
2025-11-20 15:32:34 -0800

    removed api keys

--- gtm-ai-temp/test_key.py
+++ gtm-ai-temp/test_key.py
@@ -7,7 +7,6 @@ import os
 # my key
 os.environ["OPENAI_API_KEY"] = "_key_"
 # ld key
-# os.environ["OPENAI_API_KEY"] = "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"
 client = OpenAI()
 
 resp = client.chat.completions.create(
